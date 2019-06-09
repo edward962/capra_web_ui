a3c7079477ca6aa01628f4ba15201148e72434a9
balnian <super_f_qc@hotmail.com>
2019-06-09 12:36:06 -0400

    stub for 3dviewer

---
+++ src/components/3dViewer.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script lang="tsx">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+import { rosModule } from '@/store'
+
+import * as THREE from 'three'
+
+import { OrbitControls} from '@/utils/3dviewer/OrbitControls'
+import { STLLoader } from '@/utils/3dviewer/STLLoader'
+
+
+@Component
+export default class d3Viewer extends Vue {
+  @Prop({ default: '', required: true })
+  readonly topic!: string
+
+
+}
+</script>
+
+
---
+++ types/STLLoader.d.ts
@@ -0,0 +1,18 @@
+declare module 'three-stl-loader'{
+import {
+  BufferGeometry,
+  LoadingManager
+} from 'three';
+
+
+export class STLLoader {
+  constructor(manager?: LoadingManager);
+  manager: LoadingManager;
+  path: string;
+
+  load(url: string, onLoad: (geometry: BufferGeometry) => void, onProgress?: (event: ProgressEvent) => void, onError?: (event: ErrorEvent) => void) : void;
+  setPath(path: string) : this;
+
+  parse(data: ArrayBuffer | string) : BufferGeometry;
+}
+}
