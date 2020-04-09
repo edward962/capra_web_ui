ddb2f0649d56c9b7332f4f24a1b13a1908045049
IceSentry <c.giguere42@gmail.com>
2020-04-08 23:21:34 -0400

    WIP

--- src/renderer/components/pages/Teleop.tsx
+++ src/renderer/components/pages/Teleop.tsx
@@ -1,8 +1,27 @@
 import React, { FC } from 'react'
-import { Feed } from '~components/Feed/Feed'
-import { useControl } from '~utils/hooks/useControl'
+import { Feed } from 'components/Feed/Feed'
+import { styled } from 'globalStyles/styled'
+import { controlService } from 'state/control'
+
+const StyledPIP = styled.div`
+  position: absolute;
+  width: 380px;
+  height: 200px;
+  z-index: 2;
+  left: 15px;
+  bottom: 35px;
+  /* top: 0px; */
+  /* margin-bottom: 150px; */
+`
 
 export const Teleop: FC = () => {
-  useControl('flipper')
-  return <Feed id="teleop_main" defaultFeed="camera_3d_rgb" />
+  controlService.send({ type: 'CONTROL_FLIPPER' })
+  return (
+    <>
+      <Feed id="teleop_main" defaultFeed="camera_3d_rgb" />
+      <StyledPIP>
+        <Feed id="teleop_main_2" defaultFeed="camera_3d_rgb" />
+      </StyledPIP>
+    </>
+  )
 }
